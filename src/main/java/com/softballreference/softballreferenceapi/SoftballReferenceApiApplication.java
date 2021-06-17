34ba8bc62c76697cce6362922e526f0535c08ec6
Brady Key <bradykey@gmail.com>
2021-06-16 21:24:22 -0600

    initial commit

---
+++ src/main/java/com/softballreference/softballreferenceapi/SoftballReferenceApiApplication.java
@@ -0,0 +1,13 @@
+package com.softballreference.softballreferenceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftballReferenceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftballReferenceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softballreference/softballreferenceapi/SoftballReferenceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softballreference.softballreferenceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftballReferenceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
