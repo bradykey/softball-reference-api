5ed750412212f65612d0953558edae2506b12f05
Brady Key <bradykey@gmail.com>
2021-07-05 17:53:25 -0600

    Updated the inserts to do Teams, Leagues and association TeamLeagues

--- src/main/resources/static/INSERTS.sql
+++ src/main/resources/static/INSERTS.sql
@@ -1,3 +1,4 @@
+-- TEAMS
 INSERT INTO public.team(
 	name
 )
@@ -13,15 +14,12 @@ INSERT INTO public.team(
 )
 VALUES('Sandia Baggers');
 
-INSERT INTO public.league(
-	name, season
-)
-VALUES('City', 'Spring 2018');
+-- LEAGUES
 
 INSERT INTO public.league(
 	name, season
 )
-VALUES('City', 'Fall 2018');
+VALUES('City', 'Spring 2018');
 
 INSERT INTO public.league(
 	name, season
@@ -31,48 +29,124 @@ VALUES('Sandia', 'Summer 2018');
 INSERT INTO public.league(
 	name, season
 )
+VALUES('City', 'Fall 2018');
+
+INSERT INTO public.league(
+	name, season
+)
 VALUES('City', 'Spring 2019');
+
+INSERT INTO public.league(
+	name, season
+)
+VALUES('Sandia', 'Summer 2019');
+
+INSERT INTO public.league(
+	name, season
+)
+VALUES('City', 'Summer 2019');
+
+INSERT INTO public.league(
+	name, season
+)
+VALUES('City', 'Fall 2019');
 
 INSERT INTO public.league(
 	name, season
 )
 VALUES('City', 'Summer 2021');
 
--- Garden Hosers Spring 18
-INSERT INTO public.team_league(
-	team_id, league_id
-)
-VALUES(2, 1);
+-- TEAMLEAGUE ASSOCIATIONS
 
--- Chups Fall 18
+-- Chups Fall 2018 City
 INSERT INTO public.team_league(
 	team_id, league_id
 )
-VALUES(1, 2);
+SELECT team.id, league.id 
+FROM public.team AS team, public.league AS league 
+WHERE team.name LIKE 'Chupacabras' AND league.name LIKE 'City' AND league.season LIKE 'Fall 2018';
 
--- Baggers Summer 18
+-- Chups Spring 2019 City
 INSERT INTO public.team_league(
 	team_id, league_id
 )
-VALUES(3, 3);
+SELECT team.id, league.id 
+FROM public.team AS team, public.league AS league 
+WHERE team.name LIKE 'Chupacabras' AND league.name LIKE 'City' AND league.season LIKE 'Spring 2019';
 
--- Chups Spring 19
+-- Chups Summer 2019 City
 INSERT INTO public.team_league(
 	team_id, league_id
 )
-VALUES(1, 4);
+SELECT team.id, league.id 
+FROM public.team AS team, public.league AS league 
+WHERE team.name LIKE 'Chupacabras' AND league.name LIKE 'City' AND league.season LIKE 'Summer 2019';
 
--- Chups Summer 2021
+-- Chups Fall 2019 City
 INSERT INTO public.team_league(
 	team_id, league_id
 )
-VALUES(1, 5);
+SELECT team.id, league.id 
+FROM public.team AS team, public.league AS league 
+WHERE team.name LIKE 'Chupacabras' AND league.name LIKE 'City' AND league.season LIKE 'Fall 2019';
 
--- Garden Hosers Summer 2021
+-- Chups Summer 2021 City
 INSERT INTO public.team_league(
 	team_id, league_id
 )
-VALUES(2, 5);
+SELECT team.id, league.id 
+FROM public.team AS team, public.league AS league 
+WHERE team.name LIKE 'Chupacabras' AND league.name LIKE 'City' AND league.season LIKE 'Summer 2021';
+
+-- Garden Hosers Spring 2018 City
+INSERT INTO public.team_league(
+	team_id, league_id
+)
+SELECT team.id, league.id 
+FROM public.team AS team, public.league AS league 
+WHERE team.name LIKE 'Garden Hosers' AND league.name LIKE 'City' AND league.season LIKE 'Spring 2018';
+
+-- Garden Hosers Fall 2018 City
+INSERT INTO public.team_league(
+	team_id, league_id
+)
+SELECT team.id, league.id 
+FROM public.team AS team, public.league AS league 
+WHERE team.name LIKE 'Garden Hosers' AND league.name LIKE 'City' AND league.season LIKE 'Fall 2018';
+
+-- Garden Hosers Fall 2019 City
+INSERT INTO public.team_league(
+	team_id, league_id
+)
+SELECT team.id, league.id 
+FROM public.team AS team, public.league AS league 
+WHERE team.name LIKE 'Garden Hosers' AND league.name LIKE 'City' AND league.season LIKE 'Fall 2019';
+
+-- Garden Hosers Summer 2021 City
+INSERT INTO public.team_league(
+	team_id, league_id
+)
+SELECT team.id, league.id 
+FROM public.team AS team, public.league AS league 
+WHERE team.name LIKE 'Garden Hosers' AND league.name LIKE 'City' AND league.season LIKE 'Summer 2021';
+
+-- Sandia Baggers Summer 2018 City
+INSERT INTO public.team_league(
+	team_id, league_id
+)
+SELECT team.id, league.id 
+FROM public.team AS team, public.league AS league 
+WHERE team.name LIKE 'Sandia Baggers' AND league.name LIKE 'Sandia' AND league.season LIKE 'Summer 2018';
+
+-- Sandia Baggers Summer 2018 City
+INSERT INTO public.team_league(
+	team_id, league_id
+)
+SELECT team.id, league.id 
+FROM public.team AS team, public.league AS league 
+WHERE team.name LIKE 'Sandia Baggers' AND league.name LIKE 'Sandia' AND league.season LIKE 'Summer 2019';
+
+
 
 INSERT INTO public.game(
 	date, field, opponent, opponent_score, score, team_league_id)
