37f6397ea34e987859481a0cb4b203acdaaabb42
Brady Key <bradykey@gmail.com>
2021-09-19 12:19:32 -0600

    #2: Added the @EnableJpaRepositories annotation that set the repositoryBaseClass to the implemented version of the CustomJpaRepository

--- src/main/java/com/softballreference/softballreferenceapi/SoftballReferenceApiApplication.java
+++ src/main/java/com/softballreference/softballreferenceapi/SoftballReferenceApiApplication.java
@@ -2,8 +2,12 @@ package com.softballreference.softballreferenceapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import com.softballreference.softballreferenceapi.model.repository.CustomJpaRepositoryImpl;
 
 @SpringBootApplication
+@EnableJpaRepositories(repositoryBaseClass = CustomJpaRepositoryImpl.class)
 public class SoftballReferenceApiApplication {
 
 	public static void main(String[] args) {
